6142326c9843a38ebfd034ca7f5ab34e965fcc9e
wdhenry727@gmail.com <wdhenry727@gmail.com@0c0ee571-cb06-ced3-5782-d8b83ab3feba>
2015-03-06 20:59:54 +0000

    Changes made to have 6 Hard Surface rows.  Had to rework number of additional rows as view was getting physically to large.

--- trunk/CleaningJob/grails-app/conf/DataSource.groovy
+++ trunk/CleaningJob/grails-app/conf/DataSource.groovy
@@ -19,8 +19,8 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "updaate" // one of 'create', 'create-drop', 'update', 'validate', ''
-			url = "jdbc:mysql://localhost:3306/superior_carpet"
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+			url = "jdbc:mysql://localhost:3307/superior_carpet"
             //url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
