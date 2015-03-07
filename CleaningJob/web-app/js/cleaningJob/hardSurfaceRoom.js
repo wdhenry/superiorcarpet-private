9b6b592468010c1a7194f8afd4dd9d1c6d7dbd11
Vinny.Lozada@gmail.com <Vinny.Lozada@gmail.com@0c0ee571-cb06-ced3-5782-d8b83ab3feba>
2015-03-07 23:40:45 +0000

    PS-94 - Populate Rates - Implemented Hard Surfaces section.

--- CleaningJob/web-app/js/cleaningJob/hardSurfaceRoom.js
+++ CleaningJob/web-app/js/cleaningJob/hardSurfaceRoom.js
@@ -1,6 +1,10 @@
 (function() {
   "use strict";
 
+  var SurfaceCleaningRate;
+  var SurfaceEtchedRate;
+  var SurfaceSealedWaxedRate;
+
   $(document).ready(function() {
     init();
   });
@@ -8,6 +12,16 @@
   function init() {
     // bind the change event to their respective handlers...
     $("#hsRoomCount").on("change", changeHsRoomCount);
+    $("input[id^='hsSquareFootage']").on("change", changeHsSquareFootage);
+    $("input[id^='hsEtchedCheck']").on("change", changeHsEtchedCheck);
+    $("input[id^='hsEtchedCharge']").on("change", changeHsEtchedCharge);
+    $("input[id^='hsSealedWaxedCheck']").on("change", changeHsSealedWaxedCheck);
+    $("input[id^='hsSealedWaxedCharge']").on("change", changeHsSealedWaxedCharge);
+
+    // prefetch rates for calculations...
+    SurfaceCleaningRate = RateService.getRateByName("SurfaceCleaning");
+    SurfaceEtchedRate = RateService.getRateByName("SurfaceEtched");
+    SurfaceSealedWaxedRate = RateService.getRateByName("SurfaceSealedWaxed");
 
     // invoke the change handlers for some of the controls above in order
     // to initialize the table rows based on the current selectedIndex...
@@ -22,18 +36,94 @@
 
     // Next, show the rows based on the selected index of the roomCount...
     var selectedIndex = hsRoomCount[0].selectedIndex;
-    hsRoomCount.children("option").each(
-            function() {
-              // This will make sure the first row shows when 00 is
-              // selected, but stay hidden if not...
-              if ((this.index === selectedIndex)
-                      || ((this.index > 0) && (this.index < selectedIndex))) {
-                $("#hardSurfaceRoomTable #hsRoom" + this.value).show();
-                $("#hsVisible" + this.index).val("Y");
-              } else {
-                $("#hsVisible" + this.index).val("");
-              }
-            });
+    hsRoomCount.children("option").each(function() {
+      // This will make sure the first row shows when 00 is
+      // selected, but stay hidden if not...
+      if ((this.index === selectedIndex) || ((this.index > 0) && (this.index < selectedIndex))) {
+        $("#hardSurfaceRoomTable #hsRoom" + this.value).show();
+        $("#hsVisible" + this.index).val("Y");
+      } else {
+        $("#hsVisible" + this.index).val("");
+      }
+    });
+  }
+
+  function changeHsSquareFootage(event) {
+    var suffix = getControlNameSuffix(event.currentTarget.name);
+    calculateHardSurfaceCleaningCharge(suffix);
+  }
+
+  function changeHsEtchedCheck(event) {
+
+    // get the hsEtchedCheck element that was changed...
+    var hsEtchedCheck = $(event.currentTarget);
+    var suffix = getControlNameSuffix(hsEtchedCheck[0].name);
+
+    // get the hsEtchedCharge element...
+    var hsEtchedCharge = $("#hsEtchedCharge" + suffix);
+
+    // set the rate charge accordingly...
+    if (hsEtchedCheck.is(':checked') === true) {
+      hsEtchedCharge.val(SurfaceEtchedRate.rateCharge.toFixed(2)).change();
+    } else {
+      hsEtchedCharge.val("").change();
+    }
+  }
+
+  function changeHsEtchedCharge(event) {
+    var suffix = getControlNameSuffix(event.currentTarget.name);
+    calculateHardSurfaceCleaningCharge(suffix);
+  }
+
+  function changeHsSealedWaxedCheck(event) {
+
+    // get the preVacCheck element that was changed...
+    var hsSealedWaxedCheck = $(event.currentTarget);
+    var suffix = getControlNameSuffix(hsSealedWaxedCheck[0].name);
+
+    // get the preVacCharge element...
+    var hsSealedWaxedCharge = $("#hsSealedWaxedCharge" + suffix);
+
+    // set the rate charge accordingly...
+    if (hsSealedWaxedCheck.is(':checked') === true) {
+      hsSealedWaxedCharge.val(SurfaceSealedWaxedRate.rateCharge.toFixed(2)).change();
+    } else {
+      hsSealedWaxedCharge.val("").change();
+    }
+  }
+
+  function changeHsSealedWaxedCharge(event) {
+    var suffix = getControlNameSuffix(event.currentTarget.name);
+    calculateHardSurfaceCleaningCharge(suffix);
+  }
+
+  function calculateHardSurfaceCleaningCharge(suffix) {
+
+    // only perform the calculation if the square footage is provided...
+    var hsSquareFootage = $("#hsSquareFootage" + suffix);
+    if (isNotEmpty(hsSquareFootage.val())) {
+
+      // get the field inputs...
+      var hsEtchedCharge = $("#hsEtchedCharge" + suffix);
+      var hsSealedWaxedCharge = $("#hsSealedWaxedCharge" + suffix);
+
+      // convert the input values into numerics...
+      var hsSquareFootageNum = parseFloat(hsSquareFootage.val());
+      var surfaceCleaningRateNum = parseFloat(SurfaceCleaningRate.rateCharge) || 0;
+      var hsEtchedChargeNum = parseFloat(hsEtchedCharge.val()) || 0;
+      var hsSealedWaxedChargeNum = parseFloat(hsSealedWaxedCharge.val()) || 0;
+
+      // calculate the room charge...
+      var cleaningCharge = (surfaceCleaningRateNum * hsSquareFootageNum) //
+              + (hsEtchedChargeNum * hsSquareFootageNum) //
+              + (hsSealedWaxedChargeNum * hsSquareFootageNum);
+
+      $("#hsRoomCharge" + suffix).val(cleaningCharge.toFixed(2));
+
+    } else {
+      $("#hsRoomCharge" + suffix).val("");
+    }
+
   }
 
 })();
