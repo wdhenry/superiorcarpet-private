a8c7f55fa983ba13fe086402f6fd7c150f9e76bc
Vinny.Lozada@gmail.com <Vinny.Lozada@gmail.com@0c0ee571-cb06-ced3-5782-d8b83ab3feba>
2015-03-07 21:07:52 +0000

    PS-94 Populate Rates - Implemented the room charge calculation for Additional Rooms section. In addition, I'm adding some JSHint, user-dictionary and Google's JavaScript formatter to help the code style stay consistent.

--- trunk/CleaningJob/web-app/js/cleaningJob/newJob.js
+++ trunk/CleaningJob/web-app/js/cleaningJob/newJob.js
@@ -1,133 +1,234 @@
-(function(undefined) {
-	"use strict";
-	
-	var CarpetPreVacRate;
+(/**
+ * @param undefined
+ */
+function(undefined) {
+  "use strict";
 
-	$(document).ready(function() {
-		init();
-	});
+  var CarpetCleaningRate;
+  var CarpetPreVacRate;
+  var CarpetProtectorRate;
 
-	function init() {
-		// bind the change event to their respective handlers...
-		$("#groupName").on("change", changeGroupName);
-		$("#roomCount").on("change", changeRoomCount);
-		$("#upholsteryCount").on("change", changeUpholsteryCount);
-		$("input[id^='preVacCheck']").on("change", changePreVacCheck);
-		
-		// prefetch meta data for calculations...
-		CarpetPreVacRate = getRateByName("CarpetPreVac");
+  $(document).ready(function() {
+    init();
+  });
 
-		// invoke to initialize the table rows based on the current selectedIndex
-		changeGroupName(undefined);
-		changeRoomCount(undefined);
-		changeUpholsteryCount(undefined);
-	}
+  function init() {
+    // bind the change event to their respective handlers...
+    $("#groupName").on("change", changeGroupName);
+    $("#roomCount").on("change", changeRoomCount);
+    $("#upholsteryCount").on("change", changeUpholsteryCount);
+    $("input[id^='squareFootage']").on("change", changeSquareFootage);
+    $("input[id^='preVacCheck']").on("change", changePreVacCheck);
+    $("input[id^='preVacCharge']").on("change", changePreVacCharge);
+    $("input[id^='protectorCheck']").on("change", changeProtectorCheck);
+    $("input[id^='protectorCharge']").on("change", changeProtectorCharge);
 
-	function changeGroupName(event) {
-		var groupNameElement = $("#groupName")[0];
+    // prefetch rates for calculations...
+    CarpetCleaningRate = getRateByName("CarpetCleaning");
+    CarpetPreVacRate = getRateByName("CarpetPreVac");
+    CarpetProtectorRate = getRateByName("CarpetProtector");
 
-		// First, hide all the rows
-		$("#groupRoomTable tbody tr[id^='room']").hide();
+    // invoke the change handlers for some of the controls above in order
+    // to initialize the table rows based on the current selectedIndex...
+    changeGroupName(undefined);
+    changeRoomCount(undefined);
+    changeUpholsteryCount(undefined);
+  }
 
-		// Next, show the rows based on the selected index of the groupName...
-		var selectedIndex = groupNameElement.selectedIndex;
-		switch (selectedIndex) {
-			case 5: $("#groupRoomTable #room5").show();
-			case 4: $("#groupRoomTable #room4").show();
-			case 3: $("#groupRoomTable #room3").show();
-			case 2: $("#groupRoomTable #room2").show();
-			case 1: $("#groupRoomTable #room1").show();
-				break;
-			default: $("#groupRoomTable #room0").show();
-		}
+  function changeGroupName(event) {
+    var groupNameElement = $("#groupName")[0];
 
-		// Load the rate only when the user updated it (event is defined)...
-		if (event) {
-			if (selectedIndex > 0) {
-				jQuery.ajax({
-					type : 'POST',
-					url : '/CleaningJob/roomGroup/show/' + selectedIndex + '.json',
-					success : function(data, textStatus) {
-						$("#groupRate").val(data.groupCharge);
-					},
-					error : function(XMLHttpRequest, textStatus, errorThrown) {
-						console.error(errorThrown);
-					}
-				});
-			} else {
-				$("#groupRate").val("");
-			}
-		}
-	}
+    // First, hide all the rows
+    $("#groupRoomTable tbody tr[id^='room']").hide();
 
-	function changeRoomCount(event) {
-		var roomCountElement = $("#roomCount")[0];
+    // Next, show the rows based on the selected index of the groupName...
+    var selectedIndex = groupNameElement.selectedIndex;
+    switch (selectedIndex) {
+    case 5:
+      $("#groupRoomTable #room5").show();
+    case 4:
+      $("#groupRoomTable #room4").show();
+    case 3:
+      $("#groupRoomTable #room3").show();
+    case 2:
+      $("#groupRoomTable #room2").show();
+    case 1:
+      $("#groupRoomTable #room1").show();
+      break;
+    default:
+      $("#groupRoomTable #room0").show();
+    }
 
-		// First, hide all the rows
-		$("#additionalRoomTable tbody tr[id^='room']").hide();
+    // Load the rate only when the user updated it (event is defined)...
+    if (event) {
+      if (selectedIndex > 0) {
+        $.ajax({
+          type: 'POST',
+          url: '/CleaningJob/roomGroup/show/' + selectedIndex + '.json',
+          success: function(data, textStatus) {
+            $("#groupRate").val(data.groupCharge);
+          },
+          error: function(XMLHttpRequest, textStatus, errorThrown) {
+            console.error(errorThrown);
+          }
+        });
+      } else {
+        $("#groupRate").val("");
+      }
+    }
+  }
 
-		// Next, show the rows based on the selected index of the roomCount...
-		var selectedIndex = roomCountElement.selectedIndex;
-		$("#roomCount > option").each(function() {
-			// This will make sure the first row shows when 00 is selected, but stay hidden if not...
-		    if ( (this.index === selectedIndex) || (this.index > 0 && this.index < selectedIndex) ) {
-		    	$("#additionalRoomTable #room" + this.value).show();
-		    	$("#visible0" + this.value).val("Y");
-		    } else {
-		    	$("#visible0" + this.value).val("");
-		    }
-		});
-		
-	}
+  function changeRoomCount(event) {
+    var roomCountElement = $("#roomCount")[0];
 
-	function changeUpholsteryCount(event) {
-		var upholsteryCountElement = $("#upholsteryCount")[0];
+    // First, hide all the rows
+    $("#additionalRoomTable tbody tr[id^='room']").hide();
 
-		// First, hide all the rows
-		$("#upholsteryTable tbody tr[id^='upholstery']").hide();
+    // Next, show the rows based on the selected index of the roomCount...
+    var selectedIndex = roomCountElement.selectedIndex;
+    $("#roomCount > option").each(
+            function() {
+              // This will make sure the first row shows when 00 is
+              // selected, but stay hidden if not...
+              if ((this.index === selectedIndex)
+                      || ((this.index > 0) && (this.index < selectedIndex))) {
+                $("#additionalRoomTable #room" + this.value).show();
+                $("#visible0" + this.value).val("Y");
+              } else {
+                $("#visible0" + this.value).val("");
+              }
+            });
 
-		// Next, show the rows based on the selected index of the roomCount...
-		var selectedIndex = upholsteryCountElement.selectedIndex;
-		$("#upholsteryCount > option").each(function() {
-			// This will make sure the first row shows when 00 is selected, but stay hidden if not...
-		    if ( (this.index === selectedIndex) || (this.index > 0 && this.index < selectedIndex) ) {
-		    	$("#upholsteryTable #upholstery" + this.index).show();
-		    	$("#uVisible" + this.index).val("Y");
-		    } else {
-		    	$("#uVisible" + this.index).val("");
-		    }
-		});
-		
-	}
-	
-	function changePreVacCheck(event) {
-		// get the preVacCheck element that was changed...
-		var preVacCheck = $(event.currentTarget);
-		// get the preVacCharge element...
-		var preVacCharge = preVacCheck.siblings("input[id^='preVacCharge']");
-		// set the rate charge accordingly...
-		if (preVacCheck.is(':checked') === true){
-			preVacCharge.val(CarpetPreVacRate.rateCharge.toFixed(2));
-		} else {
-			preVacCharge.val("");
-		}
-	}
-	
-	function getRateByName(rateName){
-		var result = "";
-		jQuery.ajax({
-			type : 'POST',
-			url : '/CleaningJob/rates/findByRateName.json',
-			data : { rateName : rateName},
-			async : false,
-			success : function(data, textStatus) {
-				result = data;
-			},
-			error : function(XMLHttpRequest, textStatus, errorThrown) {
-				console.error(errorThrown);
-			}
-		});
-		return result;
-	}
-	
+  }
+
+  function changeUpholsteryCount(event) {
+    var upholsteryCountElement = $("#upholsteryCount")[0];
+
+    // First, hide all the rows
+    $("#upholsteryTable tbody tr[id^='upholstery']").hide();
+
+    // Next, show the rows based on the selected index of the roomCount...
+    var selectedIndex = upholsteryCountElement.selectedIndex;
+    $("#upholsteryCount > option").each(
+            function() {
+              // This will make sure the first row shows when 00 is
+              // selected, but stay hidden if not...
+              if ((this.index === selectedIndex)
+                      || ((this.index > 0) && (this.index < selectedIndex))) {
+                $("#upholsteryTable #upholstery" + this.index).show();
+                $("#uVisible" + this.index).val("Y");
+              } else {
+                $("#uVisible" + this.index).val("");
+              }
+            });
+  }
+
+  function changeSquareFootage(event) {
+    var suffix = getControlNameSuffix(event.currentTarget.name);
+    calculateAdditionalRoomCleaningCharge(suffix);
+  }
+
+  function changePreVacCheck(event) {
+
+    // get the preVacCheck element that was changed...
+    var preVacCheck = $(event.currentTarget);
+    var suffix = getControlNameSuffix(preVacCheck[0].name);
+
+    // get the preVacCharge element...
+    var preVacCharge = $("#preVacCharge" + suffix);
+
+    // set the rate charge accordingly...
+    if (preVacCheck.is(':checked') === true) {
+      preVacCharge.val(CarpetPreVacRate.rateCharge.toFixed(2)).change();
+    } else {
+      preVacCharge.val("").change();
+    }
+  }
+
+  function changePreVacCharge(event) {
+    var suffix = getControlNameSuffix(event.currentTarget.name);
+    calculateAdditionalRoomCleaningCharge(suffix);
+  }
+
+  function changeProtectorCheck(event) {
+
+    // get the preVacCheck element that was changed...
+    var protectorCheck = $(event.currentTarget);
+    var suffix = getControlNameSuffix(protectorCheck[0].name);
+
+    // get the preVacCharge element...
+    var protectorCharge = $("#protectorCharge" + suffix);
+
+    // set the rate charge accordingly...
+    if (protectorCheck.is(':checked') === true) {
+      protectorCharge.val(CarpetProtectorRate.rateCharge.toFixed(2)).change();
+    } else {
+      protectorCharge.val("").change();
+    }
+  }
+
+  function changeProtectorCharge(event) {
+    var suffix = getControlNameSuffix(event.currentTarget.name);
+    calculateAdditionalRoomCleaningCharge(suffix);
+  }
+
+  function calculateAdditionalRoomCleaningCharge(suffix) {
+
+    // only perform the calculation if the square footage is provided...
+    var squareFootage = $("#squareFootage" + suffix);
+    if (isNotEmpty(squareFootage.val())) {
+
+      // get the field inputs...
+      var preVacCharge = $("#preVacCharge" + suffix);
+      var protectorCharge = $("#protectorCharge" + suffix);
+
+      // convert the input values into numerics...
+      var squareFootageNum = parseFloat(squareFootage.val());
+      var preVacChargeNum = parseFloat(preVacCharge.val()) || 0;
+      var protectorChargeNum = parseFloat(protectorCharge.val()) || 0;
+      var cleaningRateChargeNum = parseFloat(CarpetCleaningRate.rateCharge) || 0;
+
+      // calculate the room charge...
+      var cleaningCharge = (cleaningRateChargeNum * squareFootageNum)
+              + (preVacChargeNum * squareFootageNum)
+              + (protectorChargeNum * squareFootageNum);
+
+      var roomCharge = $("#roomCharge" + suffix);
+      roomCharge.val(cleaningCharge.toFixed(2)).change();
+    }
+  }
+
+  function isEmpty(value) {
+    return (value === undefined) || (value === null) || (value === "");
+  }
+
+  function isNotEmpty(value) {
+    return !isEmpty(value);
+  }
+
+  function getControlNameSuffix(controlName) {
+    // replace all leading non-digits characters with an empty string...
+    var suffix = controlName.replace(/^\D+/g, '');
+    return suffix;
+  }
+
+  function getRateByName(rateName) {
+    var result = "";
+    $.ajax({
+      type: 'POST',
+      url: '/CleaningJob/rates/findByRateName.json',
+      data: {
+        rateName: rateName
+      },
+      async: false,
+      success: function(data, textStatus) {
+        result = data;
+      },
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        console.error(errorThrown);
+      }
+    });
+    return result;
+  }
+
 })();
