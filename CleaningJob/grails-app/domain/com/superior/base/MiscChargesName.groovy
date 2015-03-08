debd01d3c619034b88e4a86bc25dc949017ea655
wdhenry727@gmail.com <wdhenry727@gmail.com@0c0ee571-cb06-ced3-5782-d8b83ab3feba>
2015-03-08 02:34:27 +0000

    Added rate field to MiscCharge table

--- CleaningJob/grails-app/domain/com/superior/base/MiscChargesName.groovy
+++ CleaningJob/grails-app/domain/com/superior/base/MiscChargesName.groovy
@@ -3,8 +3,10 @@ package com.superior.base
 class MiscChargesName {
 
     String miscChargesName
+	BigDecimal miscChargeRate
 	
     static constraints = {
 		miscChargesName(nullable: false)
+		miscChargeRate(nullable:true, scale: 2, min: 0.00)
     }
 }
