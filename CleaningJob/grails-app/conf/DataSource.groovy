c6f4f47bf67507ad8483f4bd1b0c3ecdc8dd007c
Vinny.Lozada@gmail.com <Vinny.Lozada@gmail.com@0c0ee571-cb06-ced3-5782-d8b83ab3feba>
2015-04-15 03:03:06 +0000

    PS-113 - Deploy App to Web Server - Deployment of CleaningJob.war was successful!

--- CleaningJob/grails-app/conf/DataSource.groovy
+++ CleaningJob/grails-app/conf/DataSource.groovy
@@ -33,7 +33,7 @@ environments {
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+			url = "jdbc:mysql://localhost:3306/superior_carpet"
             properties {
                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
                jmxEnabled = true
